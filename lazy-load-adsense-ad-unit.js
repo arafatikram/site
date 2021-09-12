24a8790a16d8bba1ae7ef54a8909131a2c77730d
Arafatikram <57841875+arafatikram@users.noreply.github.com>
2021-09-13 00:32:05 +0600

    Update lazy-load-adsense-ad-unit.js

--- lazy-load-adsense-ad-unit.js
+++ lazy-load-adsense-ad-unit.js
@@ -1,13 +1,10 @@
-<!--Lazy Load for Google AdSense Ad units-->
-<!--Replace this -->
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+// Lazy Load for Google AdSense Ad units
+// Replace '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' 
+// with either on of method below
 
 
 
-<!--Method 1. onload event -->
-<!--replace by this -->
-
-<!--noptimize-->
+// Method 1. onload event
 <script type="text/javascript">
 function downloadJSAtOnload() {
 var element = document.createElement("script");
@@ -20,12 +17,9 @@ else if (window.attachEvent)
 window.attachEvent("onload", downloadJSAtOnload);
 else window.onload = downloadJSAtOnload;
 </script>
-<!--/noptimize-->
 
 
-<!--Method 2. onscroll event-->
-<!--replace by this -->
-
+// Method 2. onscroll event
 <script type='text/javascript'>
 //<![CDATA[
 var la=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===la||0!=document.body.scrollTop&&!1===la)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),la=!0)},!0);
